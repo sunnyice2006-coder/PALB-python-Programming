191c85a035c5ac7c8b96c498d2e6096b45db1864
sunnyice2006-coder <sunnyice2006@gmail.com>
2026-02-12 23:53:26 +0530

    Create Find_Target_value.py

---
+++ Find_Target_value.py
@@ -0,0 +1,20 @@
+def find_position(nums, target):
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return left
+
+
+nums = [1, 3, 5, 6]
+target = 5
+print(find_position(nums, target))
