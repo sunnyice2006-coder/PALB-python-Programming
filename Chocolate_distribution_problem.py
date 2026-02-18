3fc0e4196e062bb9e2d2c8e9947d9908c9d43589
sunnyice2006-coder <sunnyice2006@gmail.com>
2026-02-18 09:31:34 +0530

    Create Chocolate_distribution_problem.py

---
+++ Chocolate_distribution_problem.py
@@ -0,0 +1,19 @@
+lass Solution:
+    def findMinDiff(self, arr, m):
+        n = len(arr)
+
+        if m == 0 or n == 0:
+            return 0
+
+        if n < m:
+            return -1
+
+        arr.sort()
+
+        min_diff = float('inf')
+
+        for i in range(n - m + 1):
+            diff = arr[i + m - 1] - arr[i]
+            min_diff = min(min_diff, diff)
+
+        return min_diff
